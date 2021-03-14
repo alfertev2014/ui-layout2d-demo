356dc36588813fd548da6510e385e5b0b316021b
alfertev2014 <alfertev2012@gmail.com>
2021-03-14 13:43:45 +0300

    Change example for testing.

--- layout2d/src/main/java/alfertev2014/demo/Canvas.java
+++ layout2d/src/main/java/alfertev2014/demo/Canvas.java
@@ -14,18 +14,26 @@ public class Canvas extends JPanel {
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
 
-        tree.setBounds(getBounds());
+        tree.updateLayout(getBounds());
         for (SceneNode n : tree.render().collect(Collectors.toList())) {
             n.draw(g, new Point(0, 0));
         };
     }
 
-    private final LayoutItem tree =
-            FrameNode.of(new Dimension(500, 300), Color.YELLOW, Color.BLUE,
+    private final LayoutItem tree = LayoutItem.of(BoxLayout.vertical(10,
+            FrameNode.of(new Dimension(500, 200), Color.YELLOW, Color.BLUE,
                     BoxLayout.horizontal(1,
                             TextNode.of(Color.BLACK, new Font("Serif", Font.PLAIN, 20),
-                                    "Hello!\nWorld!!!"
+                                    "Hello!"
                             )
                     )
-            );
+            ),
+            FrameNode.of(new Dimension(400, 100), Color.RED, Color.BLUE,
+                    BoxLayout.horizontal(1,
+                            TextNode.of(Color.BLACK, new Font("Monospace", Font.PLAIN, 40),
+                                    "World!!!"
+                            )
+                    )
+            )
+    ));
 }
